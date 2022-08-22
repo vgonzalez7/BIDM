2318f58b1c6993a407796517553e4ae8c7880a13
vgonzalez <vgonzalez@tlmat.unican.es>
2022-08-22 15:44:36 +0200

    Definitive version

--- gateway/server.py
+++ gateway/server.py
@@ -1,11 +1,12 @@
 #!flask/bin/python
 
-import requests, waitress
+import waitress, requests
 from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__, static_url_path="")
 api = Api(app)
+
 
 class SSmiddleware(Resource):
     def post(self):
@@ -14,12 +15,11 @@ class SSmiddleware(Resource):
        "Accept": "application/json",
        "Fiware-Service":"SmartSantander",
        "Fiware-ServicePath":"/SS"}
-      res = requests.post('http://10.10.150.229:5053/notify', headers=headers_dic, data=request.data)
-      res.close()
+      res = requests.post('http://localhost:5053/notify', headers=headers_dic, data=request.data)
       pass
 
 api.add_resource(SSmiddleware, '/SSapi', endpoint='SSapi')
 
 if __name__ == '__main__':
-    #app.run(debug=True, host='10.10.150.229', port=3000)
+    #app.run(debug=True, host='0.0.0.0', port=3000)
     waitress.serve(app, host="0.0.0.0", port=3000)
